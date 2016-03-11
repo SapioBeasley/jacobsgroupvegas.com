6a8ac6da4acc93e70107ca5d0ca8c834a1572ffa
Andreas Beasley <andreas@sapioweb.com>
2016-03-10 16:41:11 -0800

    footer include created

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,65 @@
+
+<div class="container">
+
+	<div class="col-md-3 col-sm-6">
+		<aside class="widget widget_about">
+			<h3 class="widget-title">About Us</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+		</aside>
+	</div><!-- col-md-3 -->
+
+	<div class="col-md-3 col-sm-6">
+		<aside class="widget widget_quick_links">
+			<h3 class="widget-title">Quick Links</h3>
+			<ul class="p_l_z">
+				<li><a title="Quick Links" href="#">Listing</a></li>
+				<li><a title="Quick Links" href="#">Property</a></li>
+				<li><a title="Quick Links" href="#">News</a></li>
+				<li><a title="Quick Links" href="#">Gallery</a></li>
+				<li><a title="Quick Links" href="#">Pages</a></li>
+				<li><a title="Quick Links" href="#">Types</a></li>
+				<li><a title="Quick Links" href="#">Contact</a></li>
+			</ul>
+		</aside>
+	</div>
+
+	<div class="col-md-3 col-sm-6">
+		<aside class="widget widget_address">
+			<h3 class="widget-title">Address</h3>
+			<p>108 Villa Precy Subdivision Kumintang Ilaya Batangas, Philippines</p>
+			<span>1200 666 12345</span>
+			<a title="mailto" href="mailto:jonathan@jacobsgroupvegas.com">jonathan@jacobsgroupvegas.com</a>
+		</aside>
+	</div>
+
+	<!-- col-md-3 -->
+	<div class="col-md-3 col-sm-6">
+		<aside class="widget widget_newsletter">
+			<h3 class="widget-title">NewsLetter</h3>
+			<div class="input-group">
+				<input type="text" class="form-control" placeholder="Enter Your ID">
+				<span class="input-group-btn">
+					<button class="btn btn-default" type="button">Go</button>
+				</span>
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
+		</aside>
+	</div>
+
+</div>
+
+<div id="footer-bottom" class="footer-bottom">
+	<div class="container">
+		<p class="col-md-4 col-sm-6 col-xs-12">&copy; {{date('Y')}} JacobsGroupVegas.com | Powered by <a href="https://sapioweb.com/">Sapioweb.com</a></p>
+		<div class="col-md-4 col-sm-6 col-xs-12 pull-right social">
+			<ul class="footer_social m_b_z">
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+				<li><a href="#"><i class="fa fa-rss"></i></a></li>
+			</ul>
+			<a href="#" title="back-to-top" id="back-to-top" class="back-to-top"><i class="fa fa-long-arrow-up"></i> Top</a>
+		</div>
+	</div><!-- container /- -->
+</div><!-- Footer Bottom /- -->
