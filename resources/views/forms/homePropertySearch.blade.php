f2decc201c423b2da67a173d1ae03595f8d34ca2
Andreas Beasley <andreas@sapioweb.com>
2016-03-10 16:41:36 -0800

    forms dir created to keep forms seperate

---
+++ resources/views/forms/homePropertySearch.blade.php
@@ -0,0 +1,10 @@
+{!! Form::select('property_id', ['Poperty ID','Property ID','Property ID','Property ID','Property ID','Property ID','Property ID']) !!}
+{!! Form::select('location', ['Location','Location','Location','Location','Location','Location','Location']) !!}
+{!! Form::select('type', ['Type','Type','Type','Type','Type','Type','Type']) !!}
+{!! Form::select('status', ['Status','Status','Status','Status','Status','Status','Status']) !!}
+{!! Form::select('bedrooms', ['Bedrooms','Bedrooms','Bedrooms','Bedrooms','Bedrooms','Bedrooms','Bedrooms']) !!}
+{!! Form::select('bathrooms', ['Bathrooms','Bathrooms','Bathrooms','Bathrooms','Bathrooms','Bathrooms','Bathrooms']) !!}
+{!! Form::select('min_price', ['Mn Price','Min Price','Min Price','Min Price','Min Price','Min Price','Min Price']) !!}
+{!! Form::select('max_price', ['Mx Price','Max Price','Max Price','Max Price','Max Price','Max Price','Max Price']) !!}
+{!! Form::select('min_sqft', ['Mn Sqft','Min Sqft','Min Sqft','Min Sqft','Min Sqft','Min Sqft','Min Sqft']) !!}
+{!! Form::select('max_sqft', ['Mx Sqft','Max Sqft','Max Sqft','Max Sqft','Max Sqft','Max Sqft','Max Sqft']) !!}
