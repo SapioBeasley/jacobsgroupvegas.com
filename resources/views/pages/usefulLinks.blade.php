5d4929048c4c3410cd39001142bfc5234cc6102b
Andreas Beasley <andreas@sapioweb.com>
2016-03-15 15:24:19 -0700

    userful links page complete

---
+++ resources/views/pages/usefulLinks.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.default')
+
+@section('content')
+<!-- Page Content -->
+<div class="page-content">
+	<!-- Banner Section -->
+	<div id="page-banner-section" class="page-banner-section container-fluid p_z">
+		<img src="images/aa-listing/banner.jpg" alt="banner">
+		<!-- Banner Inner -->
+		<div class="page-title">
+			<div class="container ">
+				<div class="banner-inner">
+					<h2>Useful Links</h2>
+				</div>
+			</div>
+			<div class="pages-breadcrumb">
+				<div class="container">
+					<!-- Page breadcrumb -->
+					<ol class="breadcrumb page-breadcrumb pull-right">
+						<li><a href="#">Home</a></li>
+						<li class="active">Useful Links</li>
+					</ol>
+				</div>
+			</div>
+		</div><!-- Banner Inner /- -->
+	</div><!-- Banner Section /- -->
+
+	<!-- Property Detail Page -->
+	<div class="agent-listing agent-listing2">
+		<!-- container -->
+		<div class="container">
+			<!-- container fluid -->
+			<div class="container-fluid p_z">
+				<!-- col-md-9 -->
+				<div class="col-md-12 col-sm-6 p_l_z">
+					<!-- Section Header -->
+					<div class="section-header">
+						<h3><span>USEFUL</span>Links</h3>
+					</div><!-- Section Header /- -->
+
+					<div class="row">
+						<div class="col-md-6">
+							<h4>UTILITY</h4>
+							<div class="row">
+								<div class="col-md-6">
+									<a href="http://www.nvenergy.com/">NV Energy</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.southwestgas.com/">Southwest Gas</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.lvvwd.com/">Las Vegas Valley Water District</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.republicservicesvegas.com/">Republic Services</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.cox.com/">Cox</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.directtv.com/">Directv</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.centurylink.com">Century Link</a>
+								</div>
+							</div>
+
+						</div>
+
+						<div class="col-md-6">
+							<h4>GOVERNMENT</h4>
+							<div class="row">
+								<div class="col-md-6">
+									<a href="http://www.lvccld.org">Clark County Library District</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.ccsd.net">Clark County School District</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.dmvnv.com">Nevada Department of Motor Vehicles</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.co.clark.nv.us">Clark County Nevada</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.lasvegasnevada.gov">City of Las Vegas</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.cityofnorthlasvegas.com">City of North Las Vegas</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.cityofhenderson.com">City of Henderson</a>
+								</div>
+								<div class="col-md-6">
+									<a href="http://www.bcnv.org">Boulder City</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<hr>
+
+				</div><!-- col-md-9 /- -->
+			</div><!-- container fluid /- -->
+		</div><!-- container /- -->
+	</div><!-- Agent Detail Page /- -->
+</div><!-- Page Content -->
+@endsection
