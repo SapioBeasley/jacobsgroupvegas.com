3b61e0271d0a4990c93a997f70efebfb0e274d7e
Andreas Beasley <andreas@sapioweb.com>
2016-03-14 22:48:27 -0700

    pages controller updated

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,57 +8,19 @@ use App\Http\Requests;
 
 class PagesController extends Controller
 {
-	public function showHome(Request $request = null)
+	public function __construct()
 	{
-		$config = new \PHRETS\Configuration;
-
-		$config->setLoginUrl('http://glvar.apps.retsiq.com/rets/login')
-			->setUsername('jet')
-			->setPassword('glv06')
-			->setRetsVersion('1.7.2');
-
-		$this->rets = new \PHRETS\Session($config);
-
-		$connect = $this->rets->Login();
-
-		$properties = $this->getProperties();
-
-		foreach ($properties as $propertyKey => $propertyOption) {
-			if ($propertyKey == 'all') {
-				continue;
-			}
-
-			foreach ($propertyOption as $optionKey => $option) {
-				$photos = $this->getPropertyImages($option->sysId);
-
-				$properties[$propertyKey][$optionKey]->mainImage = $photos[0];
-			}
-		}
-// dd($properties['lasVegas']);
-		return view('pages.home')->with([
-			'properties' => $properties
-		]);
+		$this->communities = \App\Community::take(24)->get();
 	}
 
-	public function getPropertyImages($listingId)
+	public function showHome(Request $request = null)
 	{
-		$photos = $this->rets->GetObject('Property', 'Photo', $listingId, '*', 0);
+		$properties = $this->getProperties();
 
-		foreach ($photos as $photo) {
-			$baseIt = base64_encode($photo->getContent());
-			// dd($baseIt);
-			$hashIt = \Hash::make($baseIt);
-			// Minimize baseit
-			// save the image
-			// get the image
-			// unminimize baseit
-			// display baseit
-
-			$photosArray[] = 'data:' . $photo->getContentType() . ';base64,' . $baseIt;
-			// dd($photosArray);
-		}
-
-		return $photosArray;
+		return view('pages.home')->with([
+			'properties' => $properties,
+			'communities' => $this->communities
+		]);
 	}
 
 	public function clearSession(Request $request) {
@@ -81,7 +43,16 @@ class PagesController extends Controller
 
 	public function showSingleProperties($listingId)
 	{
-		dd($listingId);
+		$property = \App\Property::where('listingID', '=', $listingId)->with('propertyImages')->first();
+
+		if (is_null($property)) {
+			abort(404);
+		}
+
+		return view('pages.propertyDetail')->with([
+			'property' => $property,
+			'communities' => $this->communities
+		]);
 	}
 
 	public function showBuyingServices() {
@@ -100,11 +71,11 @@ class PagesController extends Controller
 	{
 		$propertyModel = new \App\Property;
 
-		$properties['all'] = $propertyModel->with('propertyImages');
-		$properties['lasVegas'] = $propertyModel->where('city', '=', 'Las Vegas')->with('propertyImages')->get();
-		$properties['henderson'] = $propertyModel->where('city', '=', 'Henderson')->with('propertyImages')->get();
-		$properties['northLasVegas'] = $propertyModel->where('city', '=', 'North Las Vegas')->with('propertyImages')->get();
-		$properties['boulderCity'] = $propertyModel->where('city', '=', 'Boulder City')->with('propertyImages')->get();
+		$properties['all'] = $propertyModel->with('propertyImages')->orderBy('entryDate', 'DESC');
+		$properties['lasVegas'] = $propertyModel->where('city', '=', 'Las Vegas')->where('listingStatus', '!=', 'Closed')->with('propertyImages')->orderBy('entryDate', 'DESC')->get();
+		$properties['henderson'] = $propertyModel->where('city', '=', 'Henderson')->where('listingStatus', '!=', 'Closed')->with('propertyImages')->orderBy('entryDate', 'DESC')->get();
+		$properties['northLasVegas'] = $propertyModel->where('city', '=', 'North Las Vegas')->where('listingStatus', '!=', 'Closed')->with('propertyImages')->orderBy('entryDate', 'DESC')->get();
+		$properties['boulderCity'] = $propertyModel->where('city', '=', 'Boulder City')->where('listingStatus', '!=', 'Closed')->with('propertyImages')->orderBy('entryDate', 'DESC')->get();
 
 		return $properties;
 	}
