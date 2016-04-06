29f9e3f9c590951ee3edc9a8530d20f2ed5c762c
Andreas Beasley <andreas@sapioweb.com>
2016-04-06 15:07:25 -0700

    email templates

---
+++ resources/views/emails/listingPost.blade.php
@@ -0,0 +1,18 @@
+{{$user['first_name']}} {{$user['last_name']}} has listed their home.
+
+
+<strong> Email: </strong> {{$user['email']}}
+<strong>Phone:</strong> {{$user['phone']}}
+
+<hr>
+
+<strong>Address: </strong> {{$user['address1']}} {{$user['address2']}} {{$user['city']}} {{$user['state']}} {{$user['zip']}}
+<strong>Property Type: </strong> {{$user['property_type']}}
+<strong>Condition: </strong>  {{$user['condition']}}
+<strong>Beds: </strong>  {{$user['beds']}}
+<strong>Baths: </strong>  {{$user['baths']}}
+<strong>Additional Rooms: </strong>  {{$user['additional_rooms']}}
+<strong>Approx Size: </strong>  {{$user['approx_size']}}
+<strong>Approx Age of Kitchen: </strong>  {{$user['approx_age_of_kitchen']}}
+<strong>Approx Age of Baths: </strong>  {{$user['approx_age_of_baths']}}
+<strong>Message: </strong>  {{$user['message']}}
