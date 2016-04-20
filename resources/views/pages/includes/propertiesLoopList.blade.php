4779f885b8bc29bd93db882e42614614ab7f2877
Andreas Beasley <andreas@sapioweb.com>
2016-04-19 23:18:53 -0700

    more quikies

--- resources/views/pages/includes/propertiesLoopList.blade.php
+++ resources/views/pages/includes/propertiesLoopList.blade.php
@@ -1,10 +1,10 @@
 <div class="property-listing-box sale-block">
 	<!-- Property Main Box -->
 	<div class="property-main-box">
-		@if (isset($property['_source']['mainImage']['dataUri']) && ! is_null($property['_source']['mainImage']['dataUri']))
+		@if (isset($property['_source']['mainImage']) && ! is_null($property['_source']['mainImage']))
 			<div class="col-md-4 p_z property-image">
 				<a title="Property Image" href="{{route('properties.show', $property['_source']['listingId'])}}">
-					<img src="{{asset($property['_source']['mainImage']['dataUri'])}}" alt="property1" onerror="this.src='http://placehold.it/350x260?text=No Image Available'">
+					<img src="{{asset(isset($property['_source']['mainImage']['dataUri']) ? $property['_source']['mainImage']['dataUri'] : $property['_source']['mainImage'])}}" alt="property1" onerror="this.src='http://placehold.it/350x260?text=No Image Available'">
 				</a>
 			</div>
 		@else
