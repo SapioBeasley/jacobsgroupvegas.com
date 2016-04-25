3a04d915b5c1cecd126aabd34c7be14be608cdba
Andreas Beasley <andreas@sapioweb.com>
2016-04-25 14:53:28 -0700

    kernel revert until I know that chaining commands is real

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,12 +25,20 @@ class Kernel extends ConsoleKernel
 	*/
 	protected function schedule(Schedule $schedule)
 	{
-		$filePath = storage_path('app/pullResult.txt');
+		$pullFilePath = storage_path('app/pullResult.txt');
+		$removeFilePath = storage_path('app/removeResult.txt');
 
 		if (env('APP_ENV') != 'local') {
-			$schedule->command('rets:properties --function=pull')->hourly()->then(function() {
-				$this->command('rets:properties --function=remove');
-			})->sendOutputTo($filePath)->emailOutputTo(env('SCHEDULE_OUTPUT_EMAIL'));
+			$schedule->command('rets:properties --function=pull')
+			->twiceDaily(1, 13)
+			->withoutOverlapping()
+			->sendOutputTo($pullFilePath)
+			->emailOutputTo(env('SCHEDULE_OUTPUT_EMAIL'));
+
+			$schedule->command('rets:properties --function=remove')
+			->twiceDaily(3, 15)
+			->sendOutputTo($removeFilePath)
+			->emailOutputTo(env('SCHEDULE_OUTPUT_EMAIL'));
 		}
 	}
 }
